c057112bd2774c484e79fbe53008a385bf0088ac
LPastine <lucianopastine.tech@gmail.com>
2024-08-30 08:34:09 -0300

    integrate angular and spring boot for full stack

--- 02-backend/spring-boot-ecommerce/src/main/java/com/lpastine/ecommerce/dao/ProductRepository.java
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/lpastine/ecommerce/dao/ProductRepository.java
@@ -2,6 +2,8 @@ package com.lpastine.ecommerce.dao;
 
 import com.lpastine.ecommerce.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
