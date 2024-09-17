e4411a9fa1f32472f529b62fba67bdbc806ea8fb
LPastine <lucianopastine.tech@gmail.com>
2024-09-17 06:23:26 -0300

    develop app configuration

---
+++ 03-frontend/angular-ecommerce/src/app/config/my-app-config.ts
@@ -0,0 +1,8 @@
+export default {
+  oidc: {
+    clientId: '0oajpoa83hJLf4sH35d7',
+    issuer: 'https://dev-65667541.okta.com/oauth2/default',
+    redirectUri: 'http://localhost:4200/callback',
+    scopes: ['openid', 'profile', 'email'],
+  },
+};
