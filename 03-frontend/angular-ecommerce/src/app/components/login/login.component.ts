7cf2f3658cd3f169ee1c16051318427d16c031c8
LPastine <lucianopastine.tech@gmail.com>
2024-09-17 08:18:38 -0300

    adjust login call back url

--- 03-frontend/angular-ecommerce/src/app/components/login/login.component.ts
+++ 03-frontend/angular-ecommerce/src/app/components/login/login.component.ts
@@ -1,14 +1,15 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { Component, Inject, OnInit } from '@angular/core';
-import myAppConfig from '../../config/my-app-config';
 import { OKTA_AUTH } from '@okta/okta-angular';
 import { OktaAuth } from '@okta/okta-auth-js';
 import OktaSignIn from '@okta/okta-signin-widget';
 
+import myAppConfig from '../../config/my-app-config';
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css',
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
   oktaSignin: any;
@@ -33,7 +34,7 @@ export class LoginComponent implements OnInit {
     this.oktaSignin.renderEl(
       {
         el: '#okta-sign-in-widget',
-      },
+      }, // this name should be same as div tag id in login.component.html
       (response: any) => {
         if (response.status === 'SUCCESS') {
           this.oktaAuth.signInWithRedirect();
--- 03-frontend/angular-ecommerce/src/app/config/my-app-config.ts
+++ 03-frontend/angular-ecommerce/src/app/config/my-app-config.ts
@@ -2,7 +2,7 @@ export default {
   oidc: {
     clientId: '0oajpoa83hJLf4sH35d7',
     issuer: 'https://dev-65667541.okta.com/oauth2/default',
-    redirectUri: 'http://localhost:4200/callback',
+    redirectUri: 'http://localhost:4200/login/callback',
     scopes: ['openid', 'profile', 'email'],
   },
 };
